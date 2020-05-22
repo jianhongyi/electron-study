6085eba756f46cb03411e1e3b7a1b9b9c66865f8
jianyi <529518788@qq.com>
2020-05-22 08:56:26 +0800

    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+function createWindow() {
+  let win = new BrowserWindow({ width: 800, height: 600 })
+  win.loadFile('./index.html')
+//   win.webContents.openDevTools()
+  win.on('closed', () => {
+  	console.log('closed')
+  	win = null
+  })
+}
+app && app.on('ready', createWindow)
+app && app.on('window-all-closed', () => {
+	console.log('window-all-closed')
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+app && app.on('activate', () => {
+	console.log('activate')
+	if (!win) {
+		createWindow()
+	}
+})
